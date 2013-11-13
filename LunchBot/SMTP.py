57a2537f78beb4dc0bd2a11420517a45c175b092
Simon <simontomlinson@gmx.com>
2013-11-13 13:10:32 -0600

    Created new branch, changing entire SMTP module for with compatibility.

--- LunchBot/SMTP.py
+++ LunchBot/SMTP.py
@@ -1,37 +1,59 @@
 import smtplib
 
+class SMTP_Connection(smtplib.SMTP_SSL):
 
-
-class SMTP_Connection(object):
-    def __init__(self,host='smtp.gmail.com',user='altamontlunchbot@gmail.com',pwd='lunchbotpassword',port=465,ssl=True):
-        """
-        Constructs an SMTP connection class to send messages
-        @param user:
-        @param pwd:
-        @param host:
-        @param port:
-        @param ssl:
-        @return:
-        """
+    def __init__(self,host,user,pwd,port=465):
         self.host = host
         self.user = user
         self.pwd = pwd
         self.port = port
-        self.ssl = ssl
-        self.start_smtp()
+        for i in range(5):
+            try:
+                super(SMTP_Connection, self).__init__(self.host,port=self.port)
+                break
+            except smtplib.SMTPConnectError:
+                print("Error during smtp connection! Trying again (try {})"%str(i+1))
+                if i == 4:
+                    raise RuntimeError("SMTP Connection refused 5 consecutive times.")
 
-    def start_smtp(self):
-        if self.ssl:
-            conn = smtplib.SMTP_SSL(self.host,port=self.port)
-        else:
-            conn = smtplib.SMTP(self.host,port=self.port)
-        conn.login(self.user,self.pwd)
-        self.conn = conn
 
-    def send_smtp(self,to,msg): # Tries to send _msg_ to _to_. Tries to open a connection if the server isn't connected for some reason.
+    def __enter__(self):
+        print("Opening SMTP Connection to {} on port {}"%self.host,self.port)
+        return self
+
+    def login(self):
         try:
-            self.conn.sendmail(self.user,to,msg)
-        except smtplib.SMTPServerDisconnected: # SMTPServerDisconnected is raised if the server is not open for some reason (never started or timed out).
-            print('smtp connection timed out! restarting!')
-            self.start_smtp()
-            self.send_smtp(to,msg)
+            super(SMTP_Connection, self).login(self.user,self.pwd)
+        except smtplib.SMTPAuthenticationError:
+            print("Server refused login credentials!")
+
+
+    def sendmail(self,to,msg):
+        print("Sending message to {}"%to)
+        for i in range(5):
+            try:
+                super(SMTP_Connection, self).sendmail(self.user,to,msg)
+                break
+            except smtplib.SMTPAuthenticationError:
+                print("Not in [AUTH] (or something more sinister). Trying to log in again.")
+                self.login()
+            except smtplib.SMTPRecipientsRefused:
+                print("Invalid recipient address!")
+            except smtplib.SMTPDataError:
+                print("Data was refused by server!")
+            except smtplib.SMTPResponseException as exception:
+                print("Response Exception:\n"+str(exception))
+
+    def __exit__(self,type_,val,tb):
+        print("Closing SMTP Connection to {} on port {}"%self.host,self.port)
+        if type_ == smtplib.SMTPException:
+            print("Unhandled Exception")
+        self.close()
+        return True
+
+
+
+with SMTP_Connection("smtp.gmail.com","altamontlunchbot@gmail.com","lunchbotpassword") as conn:
+    conn.login()
+
+
